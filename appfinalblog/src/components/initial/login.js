05ef06b2ca108b7b28b7007fde93135cb442609e
drincastX <drincast@gmail.com>
2018-07-19 11:11:42 -0500

    en sec 90, propio, usando crypto-js, idea: realizar un tutorial

--- appfinalblog/src/components/initial/login.js
+++ appfinalblog/src/components/initial/login.js
@@ -6,12 +6,22 @@ import LoginFormEnd from './loginformend';
 
 const Login = () =>{
     const formFuction = (data) => {
-        let val =  CRYPTO.enc.Utf16.parse(data.password);
-        console.log(val.toString());
+        let valParse =  CRYPTO.enc.Utf16.parse(data.password);
+        let valstringify =  CRYPTO.enc.Utf16.stringify(data.password);
+        console.log('valParse', valParse);
+        console.log('valParse.toString()', valParse.toString());
+        console.log('valstringify', valstringify);
+        console.log('valstringify.toString()', valstringify.toString());
+
+        //convirtiendo
+        let valHex = CRYPTO.enc.Hex.parse(valParse.toString());
+        console.log('valHex', valHex, 'valHex.toString()', valHex.toString());
+        console.log('respuesta', CRYPTO.enc.Utf16.stringify(valHex));
         
-        console.log(CRYPTO.SHA3(val, {outputLength: 256}));
-        console.log(CRYPTO.enc.Utf16.stringify(val));
-        console.log(CRYPTO.SHA3(data.password).toString());
+        
+        // console.log(CRYPTO.SHA3(val, {outputLength: 256}));
+        // console.log(CRYPTO.enc.Utf16.stringify(val));
+        //console.log(CRYPTO.SHA3(data.password).toString());
         console.log("in login", data);
     }
 
