85ae55f4f75fb9f64e11fd79c038d54dec6efb0a
rdorozco <rdorozco@imbanaco.com.co>
2020-02-05 13:36:11 -0500

    Se agrega sec02 y sec04, clase 35, 36, 37

---
+++ 03ReactGuiaCompletaHooksContextMERN/sec04/35_basicos/basicos/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+import Header from './components/Header';
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
