841b91fa74b58b020ddd0dfa30889046c72064c0
drincastX <drincast@gmail.com>
2020-02-19 16:59:55 -0500

    Se continua con sec 07, creacion de style component

--- 03ReactGuiaCompletaHooksContextMERN/sec07/carinsurance/src/App.js
+++ 03ReactGuiaCompletaHooksContextMERN/sec07/carinsurance/src/App.js
@@ -5,11 +5,29 @@ import "core-js/stable";
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled';
+
+const StlDivContainer = styled.div
+`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const StlDivFormContainer = styled.div
+`
+    background-color: #fff;
+    padding: 3rem;
+`;
+
 function App() {
     return (
-        <div>
+        <StlDivContainer>
             <Header title='Cotizador de seguros'/>
-        </div>
+
+            <StlDivFormContainer>
+            
+            </StlDivFormContainer>
+        </StlDivContainer>
     );
 }
 
